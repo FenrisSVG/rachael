298b6d08909251164178863dc7832c45d65bcefe
Fenris <fsandovalchavez5@gmail.com>
2022-09-17 12:21:34 -0600

    Hola muy buenas guapisimos aqui vegetta777 comentando

--- src/components/Nosotros.vue
+++ src/components/Nosotros.vue
@@ -1,74 +1,91 @@
 <template>
-	<section class="nosotros">
-		<div class="nosotros-container" v-if="images.length > 0">
-			<div class="nosotros-container__img">
-				<div
-					class="nosotros-container__header"
-					v-for="item in images"
-					:key="item.id"
-				>
-					<img
-						:src="item.src"
-						:alt="item.alt"
-						loading="lazy"
-						class="nosotros-container__image"
-					/>
-				</div>
-                <div class="nosotros-container__text">
-                    <p class="nosotros-container__title">¡Conoce acerca de Rachael!</p>
-                </div>
-			</div>
-			<div class="nosotros-information">
-				<div class="nosotros-information__flex">
-					<div class="nosotros-information__image">
-						<i class="fa-solid fa-message nosotros-information__icon"></i>
+	<div class="nosotros-component">
+		<section class="nosotros">
+			<div class="nosotros-container" v-if="images.length > 0">
+				<div class="nosotros-container__img">
+					<div
+						class="nosotros-container__header"
+						v-for="item in images"
+						:key="item.id"
+					>
+						<img
+							:src="item.src"
+							:alt="item.alt"
+							loading="lazy"
+							class="nosotros-container__image"
+						/>
 					</div>
-					<div class="nosotros-information__text">
-						<h3 class="nosotros-information__title">
-							¿Nos Conoces?
-						</h3>
-						<p class="nosotros-information__description">
-							Somos una plataforma de <span>psicologia</span> que nace con el
-							objetivo de ayudar a las personas a informarse sobre los
-							<span>trastornos mentales</span>
-							y toda la informacion acerca de la psicologia y mantener la
-							<span>estabilidad emocional</span> que necesitan para su vida
-							diaria.
-						</p>
+					<div class="nosotros-container__text">
+						<p class="nosotros-container__title">¡Conoce acerca de Rachael!</p>
 					</div>
 				</div>
-			</div>
-		</div>
-		<div class="nosotros-valores">
-			<header class="nosotros-valores__header">
-				<h3 class="nosotros-valores__title">Nuestros Valores.</h3>
-				<div class="nosotros-valores__transform">
-					<img
-						src="../assets/svg/day67-dog.svg"
-						alt="a girl freelancer"
-						loading="lazy"
-						class="nosotros-container__image"
-					/>
+				<div class="nosotros-information">
+					<div class="wave w1">
+						<img src="../assets/svg/img2.svg" alt="ola de mar animacion" />
+					</div>
+					<div class="nosotros-information__flex">
+						<div class="nosotros-information__image">
+							<i class="fa-solid fa-message nosotros-information__icon"></i>
+						</div>
+						<div class="nosotros-information__text">
+							<h3 class="nosotros-information__title">
+								¿Nos Conoces?
+							</h3>
+							<p class="nosotros-information__description">
+								Somos una plataforma de <span>psicologia</span> que nace con el
+								objetivo de ayudar a las personas a informarse sobre los
+								<span>trastornos mentales</span>
+								y toda la informacion acerca de la psicologia y mantener la
+								<span>estabilidad emocional</span> que necesitan para su vida
+								diaria.
+							</p>
+						</div>
+					</div>
 				</div>
-			</header>
-			<footer class="nosotros-valores__footer" v-if="valores.length > 0">
-				<section class="nosotros-card" v-for="item in valores" :key="item.id">
-					<header class="nosotros-card__header">
-						<i :class="item.icon"></i>
-						<p class="nosotros-card__title">{{ item.title }}</p>
-					</header>
-					<footer class="nosotros-card__footer">
-						<p class="nosotros-card__description">{{ item.text }}</p>
-					</footer>
-				</section>
-			</footer>
-		</div>
-	</section>
+			</div>
+			<div class="nosotros-valores">
+				<header class="nosotros-valores__header">
+					<h3 class="nosotros-valores__title">Nuestros Valores.</h3>
+					<div class="nosotros-valores__transform">
+						<img
+							src="../assets/svg/day67-dog.svg"
+							alt="a girl freelancer"
+							loading="lazy"
+							class="nosotros-container__image"
+						/>
+					</div>
+				</header>
+				<footer class="nosotros-valores__footer" v-if="valores.length > 0">
+					<section class="nosotros-card" v-for="item in valores" :key="item.id">
+						<header class="nosotros-card__header">
+							<i :class="item.icon"></i>
+							<p class="nosotros-card__title">{{ item.title }}</p>
+						</header>
+						<footer class="nosotros-card__footer">
+							<p class="nosotros-card__description">{{ item.text }}</p>
+						</footer>
+					</section>
+				</footer>
+			</div>
+		</section>
+		<Footer />
+	</div>
 </template>
 
+<!-- <style scoped>
+.wave{
+	background-image: url('../assets/svg/img2.svg');
+}
+</style> -->
+
 <script>
+import Footer from "./Footer.vue";
+
 export default {
 	name: "Nosotros",
+	components: {
+		Footer,
+	},
 	data() {
 		return {
 			images: [
